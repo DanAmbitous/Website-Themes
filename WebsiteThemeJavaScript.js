9eb8368bf329affc1d6b1fc6f6b273b58d5aac99
Dan Ambitious <54007091+DanAmbitous@users.noreply.github.com>
2020-08-13 12:32:08 -0500

    Add files via upload

---
+++ WebsiteThemeJavaScript.js
@@ -0,0 +1,109 @@
+function DbuttonFunction() {
+    document.getElementById('theTitle').style.backgroundColor = '#262626';
+    document.getElementById('theTitle').style.color = 'white';
+    document.getElementById('theHeader').style.backgroundColor = '#262626';
+    document.getElementById('theHeader').style.borderColor = '#525252';
+    document.querySelector('body').style.backgroundColor = '#383838';
+    document.querySelector('main').style.color = 'white';
+    document.querySelector('footer').style.color = 'white';
+    document.getElementById('Choose-Theme').style.backgroundColor = '#262626';
+    document.getElementById('Choose-Theme').style.color = 'white';
+    document.getElementById('Choose-Theme').style.borderColor = '#525252';
+    document.getElementById('Choose-Font').style.backgroundColor = '#262626';
+    document.getElementById('Choose-Font').style.borderColor = '#525252';
+    document.getElementById('Choose-Font').style.color = 'white';
+}
+
+function LbuttonFunction() {
+    document.getElementById('theTitle').style.backgroundColor = 'white';
+    document.getElementById('theTitle').style.color = 'black';
+    document.getElementById('theHeader').style.backgroundColor = 'white';
+    document.getElementById('theHeader').style.borderColor = 'silver';
+    document.querySelector('body').style.backgroundColor = 'white';
+    document.querySelector('main').style.color = 'black';
+    document.querySelector('footer').style.color = 'black';
+    document.getElementById('Choose-Theme').style.backgroundColor = 'white';
+    document.getElementById('Choose-Theme').style.color = 'black';
+    document.getElementById('Choose-Theme').style.borderColor = 'silver';
+    document.getElementById('Choose-Font').style.backgroundColor = 'white';
+    document.getElementById('Choose-Font').style.borderColor = 'silver';
+    document.getElementById('Choose-Font').style.color = 'black';
+}
+
+function BbuttonFunction() {
+    document.getElementById('theTitle').style.backgroundColor = 'blue';
+    document.getElementById('theTitle').style.color = 'white';
+    document.getElementById('theHeader').style.backgroundColor = 'blue';
+    document.getElementById('theHeader').style.borderColor = 'lightblue';
+    document.querySelector('body').style.backgroundColor = '#0070FF';
+    document.querySelector('main').style.color = 'white';
+    document.querySelector('footer').style.color = 'white';
+    document.getElementById('Choose-Theme').style.backgroundColor = '#0036B4';
+    document.getElementById('Choose-Theme').style.color = 'white';
+    document.getElementById('Choose-Theme').style.borderColor = 'lightblue';
+    document.getElementById('Choose-Font').style.backgroundColor = 'blue';
+    document.getElementById('Choose-Font').style.borderColor = 'lightblue';
+    document.getElementById('Choose-Font').style.color = 'white';  
+}
+
+function RbuttonFunction() {
+    document.getElementById('theTitle').style.backgroundColor = 'red';
+    document.getElementById('theTitle').style.color = 'white';
+    document.getElementById('theHeader').style.backgroundColor = 'red';
+    document.getElementById('theHeader').style.borderColor = '#FF664F';
+    document.querySelector('body').style.backgroundColor = '#FF0D29';
+    document.querySelector('main').style.color = 'white';
+    document.querySelector('footer').style.color = 'white';
+    document.getElementById('Choose-Theme').style.backgroundColor = 'red';
+    document.getElementById('Choose-Theme').style.color = 'white';
+    document.getElementById('Choose-Theme').style.borderColor = '#FF9A8B';
+    document.getElementById('Choose-Font').style.backgroundColor = 'red';
+    document.getElementById('Choose-Font').style.borderColor = '#FF9A8B';
+    document.getElementById('Choose-Font').style.color = 'white';  
+}
+
+function PbuttonFunction() {
+    document.getElementById('theTitle').style.backgroundColor = 'purple';
+    document.getElementById('theTitle').style.color = 'white';
+    document.getElementById('theHeader').style.backgroundColor = 'purple';
+    document.getElementById('theHeader').style.borderColor = '#9F74FF';
+    document.querySelector('body').style.backgroundColor = '#814AFF';
+    document.querySelector('main').style.color = 'white';
+    document.querySelector('footer').style.color = 'white';
+    document.getElementById('Choose-Theme').style.backgroundColor = 'purple';
+    document.getElementById('Choose-Theme').style.color = 'white';
+    document.getElementById('Choose-Theme').style.borderColor = '#9F74FF';
+    document.getElementById('Choose-Font').style.backgroundColor = 'purple';
+    document.getElementById('Choose-Font').style.borderColor = '#9F74FF';
+    document.getElementById('Choose-Font').style.color = 'white'; 
+}
+
+function RobotoFunction() {
+    document.querySelector('main').style.fontFamily = 'Roboto';
+    document.querySelector('footer').style.fontFamily = 'Roboto';
+}
+
+function AntonFunction() {
+    document.querySelector('main').style.fontFamily = 'Anton';
+    document.querySelector('footer').style.fontFamily = 'Anton';
+}
+
+function DancingFunction() {
+    document.querySelector('main').style.fontFamily = 'Dancing Script';
+    document.querySelector('footer').style.fontFamily = 'Dancing Script';
+}
+
+function PromptFunction() {
+    document.querySelector('main').style.fontFamily = 'Prompt';
+    document.querySelector('footer').style.fontFamily = 'Prompt';
+}
+
+function PacificoFunction() {
+    document.querySelector('main').style.fontFamily = 'Pacifico';
+    document.querySelector('footer').style.fontFamily = 'Pacifico';
+}
+
+function defaultFont() {
+    document.querySelector('main').style.fontFamily = 'serif';
+    document.querySelector('footer').style.fontFamily = 'serif';
+}
